a91179f34a5ecaf0780ae1e06c5f1fe963bde55d
bedolaga66 <160242954+bedolaga66@users.noreply.github.com>
2024-11-01 00:52:10 +0500

    Добавлено меню навигации, заготовки полей регистрации и создания заявок на проведение спортивных мероприятий

---
+++ liga-tennisa/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Navbar @open-modal="showModal" />
+    <Modal :visible="isModalVisible" :title="modalTitle" @close-modal="hideModal" />
+    
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Modal,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+      modalTitle: '',
+    };
+  },
+  methods: {
+    showModal(section) {
+      this.modalTitle = section; 
+      this.isModalVisible = true; 
+    },
+    hideModal() {
+      this.isModalVisible = false; 
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #f0f0f0;
+  height: 100vh;
+  margin: 0;
+  opacity: 0;
+    animation: ani 1.5s forwards;
+  }
+  @keyframes ani {
+      0% {opacity: 0;}
+      100% {opacity: 1;}
+  }
+
+</style>
---
+++ liga-tennisa/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">Лига тенниса</div>
+    <ul class="nav-links">
+      <li><a href="#" >Список событий</a></li>
+      <li><a href="#" @click.prevent="openModal('Заявки')">Подать заявку на проведение</a></li>
+      <li><a href="#" >Статистика</a></li>
+      <li><a href="#" @click.prevent="openModal('Регистрация')">Регистрация</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MainNavbar",
+  methods: {
+    openModal(section) {
+      this.$emit('open-modal', section);
+    },
+  },
+};
+</script>
+
+  <style>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #083a07ab;
+    padding: 1rem 2rem;
+  }
+
+  .logo {
+    font-size: 1.5rem;
+    color: #fff;
+  }
+
+  .nav-links {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+  }
+
+  .nav-links li {
+    text-transform: uppercase;
+  }
+
+  .nav-links a {
+    color: #fff;
+    text-decoration: none;
+    transition: color 0.3s;
+  }
+
+  .nav-links a:hover {
+    color: #ffa500;
+  }
+
+  li:not(:last-child) { 
+    margin-right: 15px;  
+  }
+  </style>
+  
